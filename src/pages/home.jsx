a8364b1b762d6ee6625f6adbd163af22225c5607
Reem Ahmed <23-101100@students.eui.edu.eg>
2026-02-11 12:37:06 +0200

    Initial commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './home.css';
+
+const Home = () => {
+    return (
+        <>
+        
+        
+        </>
+     );
+}
+ 
+export default Home;
